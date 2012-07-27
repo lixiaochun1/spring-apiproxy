8e01c43111b62a297a4b34dc53ef0750b1a200ee
KITAZAKI Shigeru <skitazaki@gmail.com>
2012-07-27 23:51:46 +0900

    wire jdbc denpency onto service layer.

--- src/main/java/name/skitazaki/apiproxy/service/SimpleServerInfoManager.java
+++ src/main/java/name/skitazaki/apiproxy/service/SimpleServerInfoManager.java
@@ -2,17 +2,30 @@ package name.skitazaki.apiproxy.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import name.skitazaki.apiproxy.model.ServerInfo;
+import name.skitazaki.apiproxy.repository.ServerInfoDao;
 
 public class SimpleServerInfoManager implements ServerInfoManager {
 
 	private List<ServerInfo> configurations;
 
+	@Autowired
+	private ServerInfoDao serverInfoDao;
+
 	public List<ServerInfo> getConfigurations() {
-		return configurations;
+		if (serverInfoDao == null) {
+			return configurations;
+		}
+		return serverInfoDao.getServers();
 	}
 
 	public void setConfigurations(List<ServerInfo> configurations) {
 		this.configurations = configurations;
+	}
+
+	public void setServerInfoDao(ServerInfoDao serverInfoDao) {
+		this.serverInfoDao = serverInfoDao;
 	}
 }
